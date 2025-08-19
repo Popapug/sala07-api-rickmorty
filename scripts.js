6708609f54364df0718fd2e3ba72fe43a43bc49d
Popapug <popapug22@gmail.com>
2025-08-19 09:39:59 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,15 @@
+const pageInput = document.getElementById('pageInput')
+const searchButton = document.getElementById('searchBtn')
+const resultsDiv = document.getElementById('results')
+
+async function fetchCharacters(page){
+    resultsDiv.innerHTML = "<p>Carregando... </p>"
+
+    try {
+        
+    } catch (error) {
+        
+    }    
+
+
+}
